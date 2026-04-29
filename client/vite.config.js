c0adf57fca8a0571855e277c58a1504ae0a5540a
PavanCodeCraft <PavanCodeCraft@gmail.com>
2026-04-30 01:28:01 +0530

    added pwa support

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,42 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      manifest: {
+        name: 'Bijli Bill App',
+        short_name: 'Bijli Bill',
+
+        start_url: '/',
+
+        display: 'standalone',
+
+        background_color: '#0f172a',
+        theme_color: '#0f172a',
+
+        icons: [
+          {
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+
   server: {
     proxy: {
       '/api': {
